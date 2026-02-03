e0d2dfd4f9fafe12b2b9ab7cecff0cd38bbbdca5
Will M. Farr <will.farr@ligo.org>
2026-02-03 17:42:33 -0500

    Commit before I blow up some extraneous files.

--- scripts/J0740_spectral_sampling_script.jl
+++ scripts/J0740_spectral_sampling_script.jl
@@ -17,10 +17,11 @@ addprocs(Nchain)
     using Distributions
     using FITSIO
     using MCMCChainsStorage
-    using Mooncake
+#    using Mooncake
     using NCDatasets
     using PulsarLightcurveExtraction
     using Random
+    using ReverseDiff
     using StatsBase
     using Turing
 end
@@ -40,7 +41,7 @@ n_fourier = 10
 si = segment_indices(times, segment_starts, segment_ends)
 cm, sm = cos_sin_matrices(phases,n_fourier)
 
-nseg = 500 # length(segment_ends)
+nseg = 10 # length(segment_ends)
 sel = times .< segment_ends[nseg]
 
 T = sum(segment_ends[1:nseg] .- segment_starts[1:nseg])
@@ -49,26 +50,10 @@ mean_rate = sum(sel)/T
 ph = 0.0:0.01:1.0
 lc_cos_matrix, lc_sin_matrix = cos_sin_matrices(ph, n_fourier)
 
-model = varying_background_spectral_fourier_model(pi_indices[sel], si[sel], segment_starts[1:nseg], segment_ends[1:nseg], cm[sel,:], sm[sel,:], lc_cos_matrix, lc_sin_matrix)
-
-# For optimization
-avg_rate = length(si) / T
-
-mu_log_bg = log(avg_rate)
-sigma_log_bg = 1.0
-log_bg_segment = log(mean_rate) * ones(nseg)
-sigma_beta_scaled = 1.0
-beta_cos = zeros(n_fourier)
-beta_sin = zeros(n_fourier)
-bg_spec = ones(nchannels) ./ nchannels
-fg_spec = ones(nchannels, n_fourier) ./ nchannels
-
-# map_point = maximum_a_posteriori(model; initial_params=[mu_log_bg; sigma_log_bg; log_bg_segment; sigma_beta_scaled; beta_cos; beta_sin], adtype=AutoMooncake(; config=nothing))
-initial_params = [mu_log_bg; sigma_log_bg; log_bg_segment; sigma_beta_scaled; beta_cos; beta_sin; bg_spec; vec(fg_spec)]
-initial_params = [initial_params for i in 1:Nchain]
+model = PulsarLightcurveExtraction.varying_background_fourier_model(si[sel], segment_starts[1:nseg], segment_ends[1:nseg], cm[sel,:], sm[sel,:], lc_cos_matrix, lc_sin_matrix) # varying_background_spectral_fourier_model(pi_indices[sel], si[sel], segment_starts[1:nseg], segment_ends[1:nseg], cm[sel,:], sm[sel,:], lc_cos_matrix, lc_sin_matrix)
 
 # Start sampling at a point where the model won't have negative intensity
-trace = sample(model, NUTS(Nmcmc, 0.85; adtype=AutoMooncake(; config=nothing)), MCMCDistributed(), Nmcmc, Nchain; initial_params=initial_params)
+trace = sample(model, NUTS(Nmcmc, 0.85; adtype=AutoReverseDiff()), MCMCDistributed(), Nmcmc, Nchain)
 genq = generated_quantities(model, trace)
 trace = append_generated_quantities(trace, genq)
 chain = from_mcmcchains(trace; 
